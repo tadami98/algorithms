6b19cc556e250426faff6e3dc181ed117b741f69
tadami-asus <maciej98t@gmail.com>
2024-07-06 04:49:21 +0200

    fixed sum_array after failed tests + changed names of some variables to avoid reverved names

--- Poprawność algorytmów/sum.py
+++ Poprawność algorytmów/sum.py
@@ -1,23 +1,23 @@
 """ Specification and algorithm correctness proof.
 # Specification:
 
-    Name and arguments: sum(array, len)
+    Name and arguments: sum_array(array, length)
 
     Initial condition: 
         array - an array of integers.
-        len - a natural number representing the declared len of the array.
+        length - a natural number representing the declared length of the array.
 
     Final condition: 
         The algorithm should return an integer
-        that is the sum of the first len elements of this array,
+        that is the sum of the elements of this array,
         or zero if the array is empty.
 
 
 # Algorithm correctness:
 
     Termination property:
-        - The algorithm will terminate whenever i >= len.
-        - len is a constant and finite natural number.
+        - The algorithm will terminate whenever i >= length.
+        - length is a constant and finite natural number.
         - The variable i increments by 1 in each iteration.
 
     Partial correctness:
@@ -29,29 +29,30 @@
           so it will be true after any number of iterations.
 """
 
-def sum(array: list[int], len: int) -> int:
+def sum_array(array: list[int], len: int) -> int:
     """
     Return the sum of numbers in the given array.
 
     Args:
         array (list[int]): array of integers.
-        len (int): a natural number representing the declared len of the array.
+        length (int): a natural number representing the declared length of the array.
 
     Returns:
         The sum of the elements of 'array'.
-        If 'array' is empty or 'len' is 0, returns 0.
+        If 'array' is empty or 'length' is 0, returns 0.
     """
     
     sum: int = 0
     i: int = 0
 
-    while i < len and i < len(array):
+    while i < len:
         sum += array[i]
         i += 1
 
     return sum
 
 def main() -> None:
+
     array1: list[int] = [1, 2, 3]
     array2: tuple[int] = (4, 5, 6)
     array3: set[int] = {7, 8, 9}
@@ -59,6 +60,8 @@ def main() -> None:
     print(sum(array1, len(array1)))
     print(sum(array2, 2))
     print(sum(array3, len(array3)))
+
+
 
 if __name__ == "__main__":
     main()
